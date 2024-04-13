c566866822ba619b4e1ea3c685c5b9c96a8679fd
Jake Barnes <jakebarnescodes@gmail.com>
2024-04-13 17:40:20 -0500

    Enemies Attack!
    Enemies attack now. Changed the basic attack to not be CircleCast-based. Now it just deals damage when in range.

--- Assets/Scripts/Mobs/Enemies/EnemyBase.cs
+++ Assets/Scripts/Mobs/Enemies/EnemyBase.cs
@@ -12,4 +12,34 @@ public class EnemyBase : MobBase
 
         base.Start();
     }
+
+    protected override GameObject chooseTarget()
+    {
+        opponents = GameObject.FindGameObjectsWithTag("Summon");
+
+        GameObject closestOp = findClosestOpponentInTargetRange();
+        if (closestOp == null) closestOp = player;
+
+        return closestOp;
+    }
+
+    protected override void Attack()
+    {
+        /*
+        Vector3 directionOfTarget = target.transform.position - gameObject.transform.position;
+        directionOfTarget.Normalize();
+        directionOfTarget *= meleeDistance;
+
+        RaycastHit2D hit = Physics2D.CircleCast(gameObject.transform.position, meleeRadius, directionOfTarget);
+        if (hit)
+        {
+            if (hit.collider.gameObject.tag == "Summon")
+            {
+                hit.collider.gameObject.GetComponent<MobBase>().damage(meleeDamage);
+            }
+        }
+        */
+
+        base.Attack();
+    }
 }
--- Assets/Scripts/Mobs/Summons/SummonBase.cs
+++ Assets/Scripts/Mobs/Summons/SummonBase.cs
@@ -8,4 +8,31 @@ public class SummonBase : MobBase
     {
         base.Start();
     }
+
+    protected override GameObject chooseTarget()
+    {
+        opponents = GameObject.FindGameObjectsWithTag("Enemy");
+
+        return findClosestOpponentInTargetRange();
+    }
+
+    protected override void Attack()
+    {
+        /*
+        Vector3 directionOfTarget = target.transform.position - gameObject.transform.position;
+        directionOfTarget.Normalize();
+        directionOfTarget *= meleeDistance;
+
+        RaycastHit2D hit = Physics2D.CircleCast(gameObject.transform.position, meleeRadius, directionOfTarget);
+        if (hit)
+        {
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+                hit.collider.gameObject.GetComponent<MobBase>().damage(meleeDamage);
+            }
+        }
+        */
+
+        base.Attack();
+    }
 }

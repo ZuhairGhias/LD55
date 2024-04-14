5c0a4af1c274b7b53fcb479f05b298b9fbeb66dc
Jake Barnes <jakebarnescodes@gmail.com>
2024-04-14 17:27:05 -0500

    Pause Menu
    ESC pauses the game and displays controls and recipes. ESC resumes the game again.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Progress;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool Paused;
+    [SerializeField] GameObject menu;
+
+    public const string PAUSE = "Pause";
+
+    public void Start()
+    {
+        Paused = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown(PAUSE))
+        {
+            TogglePause();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Paused = true;
+        menu.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        Paused = false;
+        menu.SetActive(false);
+    }
+
+    public void TogglePause()
+    {
+        switch (Paused)
+        {
+            case true:
+                Unpause();
+                break;
+            case false:
+                Pause();
+                break;
+        }
+    }
+
+    public bool isPaused()
+    {
+        return Paused;
+    }
+}
